275833321a738e1dff5834acdb0ca36b7fcf31c0
RaihanIslamRauf <md.raufislam27@gmail.com>
2024-09-24 00:30:48 +0600

    added functional website

---
+++ js/addMoney.js
@@ -0,0 +1,23 @@
+/**
+ * How to get a number from input field
+ * 1.create a variable
+ * 2.right side document.getElementByID('id')
+ * 3. .value
+ * 4.parseFloat
+ */
+// console.log('add money loaded');
+document.getElementById('btn-add-money').addEventListener('click',function(event){
+     event.preventDefault();
+     
+    console.log('add money button clicked');
+
+    // getInputFieldValueById();
+    // const addMoney = getInputFieldValueById();
+    // console.log('add money value', addMoney);
+    //  const addMoney = document.getElementById('input-add-money').ariaValueMax;
+    //  const addMoneyNumber = parseFloat(addMoney);
+
+    const addMoney = getInputFieldValueById('input-add-money');
+    const pinNumber = getInputFieldValueById('input-pin-number');
+    console.log('add money with parameter',addMoney, pinNumber);
+});
